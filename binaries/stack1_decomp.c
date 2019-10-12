6a38dfeb4e9011399703934b060da6d65168d30d
chase1635321 <chasekanipe@gmail.com>
2019-10-12 12:16:29 -0400

    further

---
+++ binaries/stack1_decomp.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int param_1,long param_2)
+
+{
+  char local_58 [76];
+  uint local_c;
+  
+  if (param_1 == 1) {
+    errx(1,"please specify an argument\n");
+  }
+  local_c = 0;
+  strcpy(local_58,*(char **)(param_2 + 8));
+  if (local_c == 0x61626364) {
+    puts("you have correctly got the variable to the right value");
+  }
+  else {
+    printf("Try again, you got 0x%08x\n",(ulong)local_c);
+  }
+  return 0;
+}
