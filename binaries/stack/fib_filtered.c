95a3cdd6cc799a59847da7b6b52554075e5d0673
chase1635321 <chasekanipe@gmail.com>
2019-10-26 11:46:27 -0400

    auto push

---
+++ binaries/stack/fib_filtered.c
@@ -0,0 +1,32 @@
+
+void __libc_csu_init(EVP_PKEY_CTX *param_1,int param_2,int param_3)
+
+{
+  long lVar1;
+  
+  _init(param_1);
+  lVar1 = 0;
+  do {
+    (*(code *)(&__frame_dummy_init_array_entry)[lVar1])((long)param_1 & 0xffffffff,param_2,param_3)
+    ;
+    lVar1 = lVar1 + 1;
+  } while (lVar1 != 1);
+  return;
+}
+
+
+
+void __libc_csu_fini(void)
+
+{
+  return;
+}
+
+
+
+void _fini(void)
+
+{
+  return;
+}
+
---
+++ binaries/stack/filter.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+def main():
+    text = ""
+    with open("stack4_decomp.c") as f:
+        text = f.read()
+
+    text = text.split("\n")
+    i = 0
+    for line in text:
+        if "void frame_dummy(void)" in line:
+            break
+        i += 1
+    text = text[i+8:]
+
+    j = 0
+    for line in text:
+        if "void __libc_csu_init(" in line:
+            break
+        j += 1
+    text = text[:j-2]
+
+    text2 = []
+
+    for line in text:
+        temp = line
+        temp = temp.replace("uint", "int")
+        temp = temp.replace("ulong", "long")
+        temp = temp.replace("undefined8", "int")
+        text2.append(temp)
+
+    with open("fib_filtered.c", "w") as f:
+        f.write("\n".join(text2))
+
+
+    print("\n".join(text2))
+
+
+
+main()
